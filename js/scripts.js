8706dd026bcdb8819f5acdcde4a3240cc16c8da9
Mac-ziis <macg286@gmail.com>
2023-07-03 20:47:21 -0700

    Add functionality for new user inputs and separate contact information

--- js/scripts.js
+++ js/scripts.js
@@ -30,31 +30,67 @@ AddressBook.prototype.deleteContact = function(id) {
 };
 
 // Business Logic for Contacts ---------
-function Contact(firstName, lastName, phoneNumber) {
+function Contact(firstName, lastName, phoneNumber, emailAddress, emailAddressTwo, physAddress, physAddressTwo) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
+  this.emailAddress = emailAddress;
+  this.physAddress = physAddress;
+  this.emailAddressTwo = emailAddressTwo;
+  this.physAddressTwo = physAddressTwo;
 }
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 };
 
+// Contact.prototype.emails = function() {
+//   return this.emailAddress + ", " + this.emailAddressTwo;
+// };
+
+// Contact.prototype.addresses = function() {
+//   return this.physAddress + ", " + this.physAddressTwo;
+// };
+
+
+
 // User Interface Logic ---------
 let addressBook = new AddressBook();
+let workAddressBook = new AddressBook();
 
 function listContacts(addressBookToDisplay) {
+  console.log("Hey there");
   let contactsDiv = document.querySelector("div#contacts");
   contactsDiv.innerText =  null;
-  const ul = document.createElement("ul");
+  const ul = document.createElement("ul")
   Object.keys(addressBookToDisplay.contacts).forEach(function(key) {
     const contact = addressBookToDisplay.findContact(key);
     const li = document.createElement("li");
     li.append(contact.fullName());
+    li.append("," + " " + contact.phoneNumber);
+    li.append("," + " " + contact.emailAddress);
+    li.append("," + " " + contact.physAddress);
     li.setAttribute("id", contact.id);
     ul.append(li);
   });
   contactsDiv.append(ul);
+}
+
+function listWorkContacts(workAddressBookToDisplay) {
+  console.log("Hola");
+  let workDiv = document.querySelector("div#work");
+  workDiv.innerText = null;
+  const ul = document.createElement("ul");
+  Object.keys(workAddressBookToDisplay.contacts).forEach(function(key) {
+    const contact = workAddressBookToDisplay.findContact(key);
+    const li = document.createElement("li");
+    li.append(contact.fullName());
+    li.append("," + " " + contact.emailAddressTwo);
+    li.append("," + " " + contact.physAddressTwo);
+    li.setAttribute("id", contact.id);
+    ul.append(li);
+  });
+  workDiv.append(ul);
 }
 
 function handleFormSubmission(event) {
@@ -62,9 +98,15 @@ function handleFormSubmission(event) {
   const inputtedFirstName = document.querySelector("input#new-first-name").value;
   const inputtedLastName = document.querySelector("input#new-last-name").value;
   const inputtedPhoneNumber = document.querySelector("input#new-phone-number").value;
-  let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+  const inputtedEmailAddress = document.querySelector("input#new-email-address").value;
+  const inputtedSecondEmailAddress = document.querySelector("input#new-email-address2").value
+  const inputtedPhysicalAddress = document.querySelector("input#new-physical-address").value;
+  const inputtedSecondPhysicalAddress = document.querySelector("input#new-physical-address2").value;
+  let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmailAddress, inputtedSecondEmailAddress, inputtedPhysicalAddress, inputtedSecondPhysicalAddress);
   addressBook.addContact(newContact);
+  workAddressBook.addContact(newContact);
   listContacts(addressBook);
+  listWorkContacts(workAddressBook);
 }
 
 window.addEventListener("load", function (){
